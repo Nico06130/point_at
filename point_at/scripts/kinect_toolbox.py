82c50e55524fd4ae3a50e2810e6c754211935c47
Nico06130 <78531005+Nico06130@users.noreply.github.com>
2023-07-04 11:21:07 +0200

    Update kinect_toolbox.py

--- point_at/scripts/kinect_toolbox.py
+++ point_at/scripts/kinect_toolbox.py
@@ -2,27 +2,53 @@
 
 import rospy
 import mediapipe as mp
-from sensor_msgs.msg import Image, PointCloud2
+from sensor_msgs.msg import Image
 from std_msgs.msg import String, Float32
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
+from boxes_3D.srv import boxes3D
+import struct
 
 class PointAtNode:
 
     def __init__(self):
 
         self.image_sub = rospy.Subscriber('/kinect2/hd/image_color', Image, self.image_callback)
-        self.point_cloud_sub = rospy.Subscriber('/kinect2/sd/points', PointCloud2, self.pointcloud_callback)
+        self.point_cloud_sub = rospy.Subscriber('/kinect2/sd/image_depth', Image, self.image_depth_callback)
         self.bridge = CvBridge()
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_drawing_styles = mp.solutions.drawing_styles
         self.mp_hands = mp.solutions.hands
+        self.image_depth = None
 
         #Service
 
-        self.model_name = "yolov8"
-        self.model_class = 0
+        self.model_name = "yolov8m.pt"
+        self.model_class = []
+        
+        #Coordonnees
+
+        self.point_index = [0,0,0]
+        self.point_paume = [0,0,0]
+        self.point_objet = [0,0,0]
+
+
+    def image_depth_callback(self,data):
+
+        self.image_depth = data
+
+        # #Recuperation de la profondeur de la paume de la main
+
+        # depth_modified = self.bridge.imgmsg_to_cv2(data)
+        # x = 0
+        # y = 0
+        # z_value = depth_modified[y, x]
+
+        # rospy.loginfo("La profondeur de la paume vaut: ",z_value)
+
+
+
 
     def image_callback(self, req):
 
@@ -31,6 +57,7 @@ class PointAtNode:
         frame = self.bridge.imgmsg_to_cv2(req, "bgr8")
         image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         h, w, _ = frame.shape
+        self.get_3D_bboxes()
 
         with self.mp_hands.Hands(
                 model_complexity=0,
@@ -50,31 +77,33 @@ class PointAtNode:
                         self.mp_drawing_styles.get_default_hand_connections_style()
                     )
 
-                    # Print hand coordinates
-                    # for idx, landmark in enumerate(hand_landmarks.landmark):
-                    #     x = int(landmark.x * w)
-                    #     y = int(landmark.y * h)
-                    #     z = int(landmark.z * 1000)  # Scale z-coordinate for better visibility
-                    #     rospy.loginfo(f"Hand Landmark {idx}: ({x}, {y}, {z})")
 
-        cv2.imshow("Point at", frame)
-        cv2.waitKey(1)
 
-    def pointcloud_callback(self,data):
+        #cv2.imshow("Point at", frame)
+        #cv2.waitKey(1)
 
-        self.pointcloud = data
     
     def get_3D_bboxes(self):
 
-        rospy.wait_for_service('boxes3Dservice')
+        rospy.loginfo("Entree dans la callback")
+        rospy.wait_for_service('boxes_3d_service')
 
-        try:
-            get_boxes = rospy.ServiceProxy('boxes3Dservice',Boxes3D)
-            bbox = get_boxes(self.model_name,self.model_class,self.flux,self.pointcloud)
-        
-        except rospy.ServiceException as e:
+        if self.image_depth != None:
 
-            print("Service call failed: %s"%e)
+            try:
+                rospy.loginfo("Attente du service")
+                get_boxes = rospy.ServiceProxy('boxes_3d_service',boxes3D)
+                
+                bbox = get_boxes(self.model_name,self.model_class,self.flux,self.image_depth).boxes3D
+                rospy.loginfo(bbox)
+            
+            except rospy.ServiceException as e:
+
+                print("Erreur dans l'appel du service: %s"%e)
+
+        else:
+            rospy.loginfo("Pas de depth frame")
+
 
     def are_collinear(point1, point2, point3, tolerance):
         """
@@ -100,9 +129,17 @@ class PointAtNode:
         else:
             return False
         
+
+    # def get_pointed_objects(self):
+
+    #     if self.are_collinear(self.point_paume,self.point_index,self.point_objet,1):
+    #         rospy.loginfo("Les points sont coliineaires")
+
+    
     def run(self):
 
         rospy.init_node('point_at_detection')
+        
         rospy.spin()
 
 if __name__ == '__main__':
