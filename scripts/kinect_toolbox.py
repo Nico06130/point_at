22d4f7bbd4e3fefface32c4b4d30a14cc8fe8ea4
simonernst <simon.ernst@live.com>
2023-07-05 17:45:50 +0200

    add msgs and srv

--- scripts/kinect_toolbox.py
+++ scripts/kinect_toolbox.py
@@ -2,13 +2,14 @@
 
 import rospy
 import mediapipe as mp
-from sensor_msgs.msg import Image
-from std_msgs.msg import String, Float32
+from sensor_msgs.msg import Image 
+from std_msgs.msg import String, Float32, UInt16
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
 from boxes_3D.srv import boxes3D
 from yolov8_ros.srv import Yolov8
+from point_at.srv import Pointed,PointedResponse
 
 class PointAtNode:
 
@@ -17,6 +18,7 @@ class PointAtNode:
         self.image_sub = rospy.Subscriber('/kinect2/hd/image_color', Image, self.image_callback)
         self.point_at_pub = rospy.Publisher('point_at_frame',Image,queue_size=10)
         self.point_cloud_sub = rospy.Subscriber('/kinect2/hd/image_depth_rect', Image, self.image_depth_callback)
+
         self.bridge = CvBridge()
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_drawing_styles = mp.solutions.drawing_styles
@@ -25,38 +27,143 @@ class PointAtNode:
 
         #Service
 
+        self.myService = rospy.Service('Point at service', Pointed, self.myServiceCallback)
         self.model_name = "yolov8m.pt"
         self.model_class = [0]
-        
-        # #Coordonnees
+        self.MAX_ERROR = 50
 
-        self.x_index = 0
-        self.y_index = 0
-        self.z_index = 0
-        self.x_wrist = 0
-        self.y_wrist = 0
-        self.z_wrist = 0
-        self.x_objet = 0
-        self.y_objet = 0
-        self.z_objet = 0
-
-        self.landmark_coord = [] #2D coord for wrist and index
-        self.point_index = []
-        self.point_paume = []
-        self.point_objet = []
 
     def image_depth_callback(self,data):
 
         self.image_depth = data
                     
-        #else:
+    def image_callback(self,data):
 
-            #rospy.loginfo("Pas de coordonnées")
+        self.flux = data
+        vectors = self.mediapipe(data)
 
-    def image_callback(self, req):
+        if vectors != None : 
 
-        self.flux = req
-        self.draw_on_frame(req)
+            self.index = vectors[0]
+            self.wrist = vectors[1]
+
+        # self.myServiceCallback()
+
+    def mediapipe(self,image):
+
+        
+        frame = self.bridge.imgmsg_to_cv2(image, "bgr8")
+        h, w, _ = frame.shape
+
+        point_wrist = (0,0,0)
+        point_index=(0,0,0)
+
+### Dessiner les landmarks de la main ###
+
+        with self.mp_hands.Hands(
+                model_complexity=0,
+                min_detection_confidence=0.01,
+                min_tracking_confidence=0.01,
+                max_num_hands=1) as hands:
+
+            results = hands.process(frame)
+
+            if results.multi_hand_landmarks:
+
+                rospy.loginfo("Main detectee")
+
+                for hand_landmarks in results.multi_hand_landmarks:
+                    self.mp_drawing.draw_landmarks(
+                        frame,
+                        hand_landmarks,
+                        self.mp_hands.HAND_CONNECTIONS,
+                        self.mp_drawing_styles.get_default_hand_landmarks_style(),
+                        self.mp_drawing_styles.get_default_hand_connections_style()
+                    )
+
+                    x_index = hand_landmarks.landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP].x*w
+                    y_index = hand_landmarks.landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP].y*h
+                    z_index = self.get_z_landmarks(x_index,y_index)
+
+                    x_wrist = hand_landmarks.landmark[self.mp_hands.HandLandmark.WRIST].x*w
+                    y_wrist = hand_landmarks.landmark[self.mp_hands.HandLandmark.WRIST].y*h
+                    z_wrist = self.get_z_landmarks(x_wrist,y_wrist)
+
+                    point_index = (x_index,y_index,z_index)
+                    point_wrist = (x_wrist,y_wrist,z_wrist)
+
+                    landmarks = (point_index,point_wrist)
+
+                    return landmarks
+
+            else:
+
+                landmarks = None
+
+            return landmarks
+        
+                # cv2.rectangle(frame, (int(self.x1_rect), int(self.y1_rect)), (int(self.x2_rect), int(self.y2_rect)), (0, 255, 0), 2)
+
+                # if point_wrist != (0,0,0) and point_index != (0,0,0):
+
+                #     if self.are_collinear_espace(point_wrist,point_index,objet,5) :
+
+                #         rospy.loginfo("Pointe vers:",boite.bbox_class)
+                #         cv2.rectangle(frame, (int(self.x1_rect), int(self.y1_rect)), (int(self.x2_rect), int(self.y2_rect)), (0, 255, 0), -1)
+                    
+
+    def myServiceCallback(self,req):
+
+
+        #rospy.loginfo("Request: "+str(req))
+        rate = rospy.Rate(10) # 10hz
+        isEnd = False
+        error_count = 0
+
+        while not rospy.is_shutdown() or not isEnd or error_count<self.MAX_ERROR: 
+
+        # get the current vector self._vector 
+
+            point1 = self.wrist
+            point2 = self.index
+
+        # service call boxes_3d
+
+            try:
+
+                rospy.loginfo("Attente du service boxes 3D")
+
+                get_boxes_espace = rospy.ServiceProxy('boxes_3d_service',boxes3D)             
+                bbox_espace = get_boxes_espace(self.model_name,self.model_class,self.flux,self.image_depth).boxes3D
+
+                for boite in bbox_espace:
+
+                    x1_rect = boite.xmin
+                    x2_rect = boite.xmax
+                    y1_rect = boite.ymin
+                    y2_rect = boite.ymax
+                    classe = boite.bboxlass
+                    id_class = boite.ID
+
+                    x_objet =  (self.x1_rect+ self.x2_rect)/2
+                    y_objet = (self.y1_rect + self.y2_rect)/2
+                    z_objet = boite.centerz /100 #en cm
+
+                    point3 = (x_objet,y_objet,z_objet)
+
+                    if self.are_collinear_espace(point1,point2,point3):
+                        rospy.loginfo("Pointe vers: %s",classe)
+                        isEnd = True
+
+            except rospy.ServiceException as e:
+
+                print("erreur d'appel du service")
+
+            error_count +=1
+
+            rate.sleep()
+
+        #self.point_at_pub.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
 
     def are_collinear_espace(self,point1, point2, point3, tolerance):
         """
@@ -82,168 +189,23 @@ class PointAtNode:
         else:
             return False
 
-    def draw_on_frame(self,image):
-
-        
-        frame = self.bridge.imgmsg_to_cv2(image, "bgr8")
-        frame_copy = frame
-        h, w, _ = frame.shape
-
-        point_wrist = (0,0,0)
-        point_index=(0,0,0)
-        rospy.wait_for_service('boxes_3d_service')
-
-        if self.image_depth != None:
-
-            try:
-                #rospy.loginfo("Attente du service boxes 3D")
-
-                get_boxes_espace = rospy.ServiceProxy('boxes_3d_service',boxes3D)             
-                bbox_espace = get_boxes_espace(self.model_name,self.model_class,self.flux,self.image_depth).boxes3D
-
-
-### Dessiner les landmarks de la main ###
-
-                with self.mp_hands.Hands(
-                        model_complexity=0,
-                        min_detection_confidence=0.01,
-                        min_tracking_confidence=0.01,
-                        max_num_hands=1) as hands:
-
-                    results = hands.process(frame)
-
-                    if results.multi_hand_landmarks:
-
-                        rospy.loginfo("Main detectee")
-
-                        for hand_landmarks in results.multi_hand_landmarks:
-                            self.mp_drawing.draw_landmarks(
-                                frame,
-                                hand_landmarks,
-                                self.mp_hands.HAND_CONNECTIONS,
-                                self.mp_drawing_styles.get_default_hand_landmarks_style(),
-                                self.mp_drawing_styles.get_default_hand_connections_style()
-                            )
-
-                            x_index = hand_landmarks.landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP].x*w
-                            y_index = hand_landmarks.landmark[self.mp_hands.HandLandmark.INDEX_FINGER_TIP].y*h
-                            rospy.loginfo(x_index)
-                            z_index = self.get_z_landmarks(x_index,y_index)
-                            point_index = (x_index,y_index,z_index)
-
-                            x_wrist = hand_landmarks.landmark[self.mp_hands.HandLandmark.WRIST].x*w
-                            y_wrist = hand_landmarks.landmark[self.mp_hands.HandLandmark.WRIST].y*h
-                            z_wrist = self.get_z_landmarks(x_wrist,y_wrist)
-
-                            point_wrist = (x_wrist,y_wrist,z_wrist)
-
-### Dessiner les boundingbox des objets ###
-
-                for boite in bbox_espace:
-
-                    self.x1_rect = boite.xmin
-                    self.x2_rect = boite.xmax
-                    self.y1_rect = boite.ymin
-                    self.y2_rect = boite.ymax
-
-                    self.x_objet =  (self.x1_rect+ self.x2_rect)/2
-                    self.y_objet = (self.y1_rect + self.y2_rect)/2
-                    self.z_objet = boite.centerz /100 #en cm
-
-                    objet = (self.x_objet,self.y_objet,self.z_objet)
-                    #rospy.loginfo(self.z_objet)
-
-                    cv2.rectangle(frame, (int(self.x1_rect), int(self.y1_rect)), (int(self.x2_rect), int(self.y2_rect)), (0, 255, 0), 2)
-
-                    if point_wrist != (0,0,0) and point_index != (0,0,0):
-
-                        if self.are_collinear_espace(point_wrist,point_index,objet,5) :
-
-                            rospy.loginfo("Pointe vers:",boite.bbox_class)
-                            cv2.rectangle(frame, (int(self.x1_rect), int(self.y1_rect)), (int(self.x2_rect), int(self.y2_rect)), (0, 255, 0), -1)
-                        
-
-
-            except rospy.ServiceException as e:
-
-                print("erreur d'appel du service")
-
-
-                    #rospy.loginfo(point_wrist)
-
-        else:
-
-            rospy.loginfo("Pas de depth frame")
-
-
-        # Appel du service boxes_3d_service pour recuperer les coordonnes 3D des boundingbpx
-
-        # rospy.loginfo("Entree dans la callback d'appel de boxes 3D")
-        # rospy.wait_for_service('boxes_3d_service')
-
-        # if self.image_depth != None:
-        #     try:
-        #         #rospy.loginfo("Attente du service boxes 3D")
-
-        #         get_boxes_espace = rospy.ServiceProxy('boxes_3d_service',boxes3D)             
-        #         bbox_espace = get_boxes_espace(self.model_name,self.model_class,self.flux,self.image_depth).boxes3D
-
-        # ### Dessiner les boundingbox des objets ###
-
-        #         for boite in bbox_espace:
-
-        #             self.x1_rect = boite.xmin
-        #             self.x2_rect = boite.xmax
-        #             self.y1_rect = boite.ymin
-        #             self.y2_rect = boite.ymax
-
-        #             self.x_objet =  (self.x1_rect+ self.x2_rect)/2
-        #             self.y_objet = (self.y1_rect + self.y2_rect)/2
-        #             self.z_objet = boite.centerz /100 #en cm
-
-        #             #rospy.loginfo(self.z_objet)
-        #             cv2.rectangle(frame, (int(self.x1_rect), int(self.y1_rect)), (int(self.x2_rect), int(self.y2_rect)), (0, 255, 0), 2)
-         
-        #     except rospy.ServiceException as e:
-
-        #         print("Erreur dans l'appel du service: %s"%e)
-
-
-        #     #rospy.loginfo(point_wrist)
-
-        # else:
-
-        #     rospy.loginfo("Pas de depth frame")
-
-                
-                    #rospy.loginfo(self.landmark_coord)
-
-                    # rospy.loginfo("Coords de lindex:",self.landmark_coord[0])
-
-        self.point_at_pub.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
-
-
     def get_z_landmarks(self,coordx,coordy):
         """
         Calcul de la profondeur avec le pointcloud
         """
 
-        if coordx != None and coordy != None:
-        
-            depth_array=self.bridge.imgmsg_to_cv2(self.image_depth,"16UC1")
-            z_coord=depth_array[int(coordy)][int(coordx)]
-        #rospy.loginfo(z_coord)
+        depth_array=self.bridge.imgmsg_to_cv2(self.image_depth,"16UC1")
+        z_coord=depth_array[int(coordy)][int(coordx)]
 
-            if z_coord==0:
-                z_coord=-1
+        if z_coord==0:
+            z_coord=-1
 
         else:
 
             z_coord = -1
 
-        return z_coord
-        
-    
+        return z_coord        
+
 
     def run(self):
 
@@ -257,10 +219,3 @@ if __name__ == '__main__':
         node.run()
     except rospy.ROSInterruptException:
         pass
-
-# # Print hand coordinates
-# for idx, landmark in enumerate(hand_landmarks.landmark):
-#     x = int(landmark.x * w)
-#     y = int(landmark.y * h)
-#     z = int(landmark.z * 1000)  # Scale z-coordinate for better visibility
-#     rospy.loginfo(f"Hand Landmark {idx}: ({x}, {y}, {z})")
