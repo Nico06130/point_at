618320966bad2cb7b226fd6559768641fe432c66
mathieuVial <mathieu.vial@cpe.fr>
2023-07-07 17:40:37 +0200

    package update

--- point_at_node/scripts/call_point_at_service.py
+++ point_at_node/scripts/call_point_at_service.py
@@ -1,65 +1,46 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import rospy
 from sensor_msgs.msg import Image
 from std_msgs.msg import String, Float32
-from cv_bridge import CvBridge
 import cv2
 import numpy as np
-import mediapipe as mp
+from point_at_srvs.srv import PointedId
 
-class TestNode:
+class PointAtNode:
 
     def __init__(self):
 
-        self.image_sub = rospy.Subscriber('/kinect2/hd/image_color', Image, self.callback)
-        self.point_at_pub = rospy.Publisher('mediapipe_test',Image,queue_size=10)
-        self.bridge = CvBridge()
-        self.mp_drawing = mp.solutions.drawing_utils
-        self.mp_drawing_styles = mp.solutions.drawing_styles
-        self.mp_hands = mp.solutions.hands
-        
-    def callback(self,req):
-	
-        frame = self.bridge.imgmsg_to_cv2(req, "bgr8")
-        
-        with self.mp_hands.Hands(
-                model_complexity=0,
-                min_detection_confidence=0.1,
-                min_tracking_confidence=0.1,
-                max_num_hands=1) as hands:
+        rospy.init_node("point_at_service_call",anonymous=True)
+        self.model_name=""
+        self.model_class=[0]
+        self.image=Image()
 
-            results = hands.process(frame)
+    def call(self):
 
-            if results.multi_hand_landmarks:
+        rospy.wait_for_service('point_at_service')
 
-                for hand_landmarks in results.multi_hand_landmarks:
-                    self.mp_drawing.draw_landmarks(
-                        frame,
-                        hand_landmarks,
-                        self.mp_hands.HAND_CONNECTIONS,
-                        self.mp_drawing_styles.get_default_hand_landmarks_style(),
-                        self.mp_drawing_styles.get_default_hand_connections_style()
-                    )
-                    rospy.loginfo(hand_landmarks)
+        try:
 
-            else:
+            rospy.loginfo("Attente du service Point At")
 
-                rospy.loginfo("pas de main dans le champ")
-                    
-        #cv2.imshow("test",frame)
-        self.point_at_pub.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
-        
+            get_service = rospy.ServiceProxy('point_at__service',PointedId)  
+            id_pointe = get_service().pointed_msg
+            rospy.loginfo(id_pointe)
 
-    def run(self):
+        except rospy.ServiceException as e:
 
-        rospy.init_node('point_at_detection')
-        
-        rospy.spin()
-        
-if __name__ == '__main__':
-    try:
-        node = TestNode()
-        node.run()
-    except rospy.ROSInterruptException:
-        pass
+                print("Erreur dans l'appel du service: %s"%e)
+
+
+if __name__=="__main__":
+
+    if __name__ == '__main__':
+
+        a=PointAtNode()
+        a.call()
+
+        while not rospy.is_shutdown():
+            
+            a.test()
+            rospy.sleep(1)
